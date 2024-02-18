3049f22583c16cfca963b4809d8d0805b18e0543
paulmothapo <paulmothapo.personal@gmail.com>
2024-02-18 17:56:31 +0200

    postpage update

--- components/PostPage.tsx
+++ components/PostPage.tsx
@@ -46,7 +46,7 @@ export default function PostPage(props: PostPageProps) {
 
       <Layout preview={preview} loading={loading}>
         <Container>
-          <BlogHeader title={title} level={3} />
+          <BlogHeader title={title} level={2} />
           {preview && !post ? (
             <PostTitle>Loading…</PostTitle>
           ) : (
